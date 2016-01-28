592a4a9280fb8cfce1ea8eaf916f252435829ff8
Dauren Amanbayev <daur_aman@mail.ru>
2016-01-28 17:19:47 +0600

    Change data annotations

--- RemoteWork/RemoteData/RemoteTask.cs
+++ RemoteWork/RemoteData/RemoteTask.cs
@@ -17,7 +17,7 @@ namespace RemoteWork.Data
         [Key]
         public int Id { get; set; }
         [Required, MaxLength(50)]
-        public string Name { get; set; }
+        public string TaskName { get; set; }
         [MaxLength(256)]
         public string Description { get; set; }
         public DateTime? Date { get; set; }
