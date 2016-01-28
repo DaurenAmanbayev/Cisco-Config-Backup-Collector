8017591963b89868360e58ed8092072cd93be10f
Dauren Amanbayev <daur_aman@mail.ru>
2016-01-28 17:17:37 +0600

    Category class structure changed

---
+++ RemoteWork/RemoteData/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteWork.Data
+{
+    public class Category
+    {
+        public Category()
+        {
+            Favorites = new HashSet<Favorite>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(50)]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Favorite> Favorites { get; set; }
+    }
+}
--- RemoteWork/RemoteWorkUtil/Program.cs
+++ RemoteWork/RemoteWorkUtil/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RemoteWork.Access;
+using RemoteWork.Data;
+using System.Data.Entity;
 
 namespace RemoteWorkUtil
 {
@@ -10,6 +13,92 @@ namespace RemoteWorkUtil
     {
         static void Main(string[] args)
         {
+            //Database.SetInitializer(new Init());
+           // Test();
+            using (RconfigContext ctx = new RconfigContext())
+            {
+                Console.WriteLine(ctx.Categories.Count());
+                foreach (Category cat in ctx.Categories)
+                {
+                    Console.WriteLine(cat.CategoryName);
+                }
+                Console.WriteLine(ctx.Protocols.Count());
+                foreach (Protocol prot in ctx.Protocols)
+                {
+                    Console.WriteLine("Protocol Name: {0} DefPort: {1}", prot.Name, prot.DefaultPort);
+                }
+            }
+            Console.WriteLine("CHECKED");
+            Console.ReadKey();
+        }
+
+        static void FirstInsert()
+        {
+            using (RconfigContext context = new RconfigContext())
+            {
+                //PROTOCOL
+                context.Protocols.Add(new Protocol
+                {
+                    Name = "SSH",
+                    DefaultPort = 22
+                });
+                context.Protocols.Add(new Protocol
+                {
+                    Name = "Telnet",
+                    DefaultPort = 23
+                });
+
+                //Categories
+                Category routers = new Category
+                {
+                    CategoryName = "Routers"
+                };
+
+                Category switches = new Category
+                {
+                    CategoryName = "Switches"
+                };
+
+                Category servers = new Category
+                {
+                    CategoryName = "Servers"
+                };
+
+                context.Categories.Add(routers);
+                context.Categories.Add(switches);
+                context.Categories.Add(servers);
+
+                //COMMANDS
+                ICollection<Category> cisco = new HashSet<Category>();
+                cisco.Add(routers);
+                cisco.Add(switches);
+
+                context.Commands.Add(new Command
+                {
+                    Name = "show running-config",
+                    Categories = cisco
+                });
+                ICollection<Category> vlan = new HashSet<Category>();
+                vlan.Add(switches);
+                context.Commands.Add(new Command
+                {
+                    Name = "show ip vlan brief",
+                    Categories = vlan
+                });
+
+                //CREDENTIALS
+                context.Credentials.Add(new Credential
+                {
+                    CredentialName = "Default",
+                    Username = "root",
+                    Domain = "domain.com",
+                    Password = "toor"
+                });
+
+                context.SaveChanges();
+            }
         }
     }
+
+
 }
