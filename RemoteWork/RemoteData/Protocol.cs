d1c871f38124e50ea8e98acac72ed54cc486636a
Dauren Amanbayev <daur_aman@mail.ru>
2016-01-28 17:19:22 +0600

    Add default port properties

--- RemoteWork/RemoteData/Protocol.cs
+++ RemoteWork/RemoteData/Protocol.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RemoteWork.Data
 {
-    public enum Protocol
+    public class Protocol
     {
-        Telnet,
-        SSH
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int DefaultPort { get; set; }
     }
 }
