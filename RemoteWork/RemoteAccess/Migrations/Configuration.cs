fddf6e2ff7fd1689e5779d44f082e10445c6cdd0
Dauren Amanbayev <daur_aman@mail.ru>
2016-01-28 17:20:45 +0600

    Comment seed methods

--- RemoteWork/RemoteAccess/Migrations/Configuration.cs
+++ RemoteWork/RemoteAccess/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace RemoteWork.Access.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using RemoteWork.Data;
+    using System.Collections.Generic;
 
     public  sealed class Configuration : DbMigrationsConfiguration<RemoteWork.Access.RconfigContext>
     {
@@ -14,20 +16,21 @@ namespace RemoteWork.Access.Migrations
             AutomaticMigrationDataLossAllowed = true; 
         }
 
-        protected override void Seed(RemoteWork.Access.RconfigContext context)
-        {
-            //  This method will be called after migrating to the latest version.
+        //protected override void Seed(RemoteWork.Access.RconfigContext context)
+        //{            
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+        //    //  This method will be called after migrating to the latest version.
+
+        //    //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+        //    //  to avoid creating duplicate seed data. E.g.
+        //    //
+        //    //    context.People.AddOrUpdate(
+        //    //      p => p.FullName,
+        //    //      new Person { FullName = "Andrew Peters" },
+        //    //      new Person { FullName = "Brice Lambson" },
+        //    //      new Person { FullName = "Rowan Miller" }
+        //    //    );
+        //    //
+        //}
     }
 }
