f77cda23cf5ddc22691b175508e0647703cadfb4
Dauren Amanbayev <daur_aman@mail.ru>
2016-01-28 12:50:18 +0600

    Command Id changed

--- RemoteWork/RemoteData/Command.cs
+++ RemoteWork/RemoteData/Command.cs
@@ -11,8 +11,8 @@ namespace RemoteWork.Data
     {
         [Key]
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; } 
+        [Required, MaxLength(50)]
+        public string Name { get; set; }       
         
     }
 }
