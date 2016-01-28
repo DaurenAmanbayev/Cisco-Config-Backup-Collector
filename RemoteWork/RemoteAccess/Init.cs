5d8ede5c519d20bf83290ed46fb849ca1b79fe7f
Dauren Amanbayev <daur_aman@mail.ru>
2016-01-28 17:20:25 +0600

    Comment init

--- RemoteWork/RemoteAccess/Init.cs
+++ RemoteWork/RemoteAccess/Init.cs
@@ -4,29 +4,77 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RemoteWork.Data;
 
 namespace RemoteWork.Access
 {
-    class Init : DropCreateDatabaseIfModelChanges<RconfigContext>
+    public class Init : DropCreateDatabaseIfModelChanges<RconfigContext>
     {
         protected override void Seed(RconfigContext context)
         {
             base.Seed(context);
 
-            //var office806 = new Location { LocationName = "Office806" };
-            //context.Attendees.Add(new Attendee
+            ////PROTOCOL
+            //context.Protocols.Add(new Protocol
+            //    {
+            //        Name = "SSH",
+            //        DefaultPort = 22
+            //    });
+            //context.Protocols.Add(new Protocol
+            //    {
+            //        Name = "Telnet",
+            //        DefaultPort = 23
+            //    });
+
+            ////Categories
+            //Category routers = new Category
+            //    {
+            //        CategoryName = "Routers"
+            //    };
+
+            //Category switches = new Category
             //{
-            //    DateAdded = DateTime.UtcNow,
-            //    LastName = "Ivanov",
-            //    Location = office806
-            //});
-            //context.Attendees.Add(new Attendee
+            //    CategoryName = "Switches"
+            //};
+
+            //Category servers = new Category
             //{
-            //    DateAdded = DateTime.UtcNow,
-            //    LastName = "Petrov",
-            //    Location = office806
+            //    CategoryName = "Servers"
+            //};
+
+            //context.Categories.Add(routers);
+            //context.Categories.Add(switches);
+            //context.Categories.Add(servers);
+
+            ////COMMANDS
+            //ICollection<Category> cisco = new HashSet<Category>();
+            //cisco.Add(routers);
+            //cisco.Add(switches);
+
+            //context.Commands.Add(new Command
+            //{
+            //    Name = "show running-config",
+            //    Categories = cisco
             //});
+            //ICollection<Category> vlan = new HashSet<Category>();
+            //vlan.Add(switches);
+            //context.Commands.Add(new Command
+            //{
+            //    Name = "show ip vlan brief",
+            //    Categories = vlan
+            //});
+
+            ////CREDENTIALS
+            //context.Credentials.Add(new Credential
+            //{
+            //    CredentialName = "Default",
+            //    Username = "root",
+            //    Domain = "domain.com",
+            //    Password = "toor"
+            //});
+
             //context.SaveChanges();
+
         }
     }
 }
