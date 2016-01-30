e41c59717eceb646928c369f16ae70788e41f3b2
Dauren Amanbayev <daur_aman@mail.ru>
2016-01-30 11:54:47 +0600

    Category Manager unload error resolving

--- RemoteWork/RemoteWork/Managers/Category Manager.Designer.cs
+++ RemoteWork/RemoteWork/Managers/Category Manager.Designer.cs
@@ -29,72 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.listBoxCategories = new System.Windows.Forms.ListBox();
-            this.contextMenuStripCategories = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBoxCategory = new System.Windows.Forms.ListBox();
+            this.contextMenuStripCategory = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.addCategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editCategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.deleteCategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStripCategories.SuspendLayout();
+            this.contextMenuStripCategory.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBoxCategories
+            // listBoxCategory
             // 
-            this.listBoxCategories.ContextMenuStrip = this.contextMenuStripCategories;
-            this.listBoxCategories.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBoxCategories.FormattingEnabled = true;
-            this.listBoxCategories.Location = new System.Drawing.Point(0, 0);
-            this.listBoxCategories.Name = "listBoxCategories";
-            this.listBoxCategories.Size = new System.Drawing.Size(254, 332);
-            this.listBoxCategories.TabIndex = 0;
+            this.listBoxCategory.ContextMenuStrip = this.contextMenuStripCategory;
+            this.listBoxCategory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxCategory.FormattingEnabled = true;
+            this.listBoxCategory.Location = new System.Drawing.Point(0, 0);
+            this.listBoxCategory.Name = "listBoxCategory";
+            this.listBoxCategory.Size = new System.Drawing.Size(254, 332);
+            this.listBoxCategory.TabIndex = 0;
             // 
-            // contextMenuStripCategories
+            // contextMenuStripCategory
             // 
-            this.contextMenuStripCategories.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextMenuStripCategory.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addCategoryToolStripMenuItem,
             this.editCategoryToolStripMenuItem,
             this.deleteCategoryToolStripMenuItem});
-            this.contextMenuStripCategories.Name = "contextMenuStripCategories";
-            this.contextMenuStripCategories.Size = new System.Drawing.Size(159, 70);
+            this.contextMenuStripCategory.Name = "contextMenuStripCategory";
+            this.contextMenuStripCategory.Size = new System.Drawing.Size(159, 70);
             // 
             // addCategoryToolStripMenuItem
             // 
             this.addCategoryToolStripMenuItem.Name = "addCategoryToolStripMenuItem";
             this.addCategoryToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
             this.addCategoryToolStripMenuItem.Text = "Add Category";
-            this.addCategoryToolStripMenuItem.Click += new System.EventHandler(this.addCategoryToolStripMenuItem_Click);
             // 
             // editCategoryToolStripMenuItem
             // 
             this.editCategoryToolStripMenuItem.Name = "editCategoryToolStripMenuItem";
             this.editCategoryToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
             this.editCategoryToolStripMenuItem.Text = "Edit Category";
-            this.editCategoryToolStripMenuItem.Click += new System.EventHandler(this.editCategoryToolStripMenuItem_Click);
             // 
             // deleteCategoryToolStripMenuItem
             // 
             this.deleteCategoryToolStripMenuItem.Name = "deleteCategoryToolStripMenuItem";
             this.deleteCategoryToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
             this.deleteCategoryToolStripMenuItem.Text = "Delete Category";
-            this.deleteCategoryToolStripMenuItem.Click += new System.EventHandler(this.deleteCategoryToolStripMenuItem_Click);
             // 
             // Category_Manager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(254, 332);
-            this.Controls.Add(this.listBoxCategories);
+            this.Controls.Add(this.listBoxCategory);
             this.Name = "Category_Manager";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Category_Manager";
-            this.contextMenuStripCategories.ResumeLayout(false);
+            this.contextMenuStripCategory.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBoxCategories;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStripCategories;
+        private System.Windows.Forms.ListBox listBoxCategory;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripCategory;
         private System.Windows.Forms.ToolStripMenuItem addCategoryToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem editCategoryToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem deleteCategoryToolStripMenuItem;
