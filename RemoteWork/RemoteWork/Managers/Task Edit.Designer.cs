66759b250bc22ee17f47ed89a96b71c40122921b
Dauren Amanbayev <daur_aman@mail.ru>
2016-02-01 17:07:42 +0600

    Task Edit added

---
+++ RemoteWork/RemoteWork/Managers/Task Edit.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace RemoteWork.Managers
+{
+    partial class Task_Edit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDesc = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBoxFavorites = new System.Windows.Forms.CheckedListBox();
+            this.buttonAddFav = new System.Windows.Forms.Button();
+            this.flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBoxCommands = new System.Windows.Forms.CheckedListBox();
+            this.buttonAddCommand = new System.Windows.Forms.Button();
+            this.flowLayoutPanel8 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.flowLayoutPanel5.SuspendLayout();
+            this.flowLayoutPanel8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel8, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 189F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 176F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(340, 490);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.textBoxName);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(334, 29);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Task Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(71, 3);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(185, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.label4);
+            this.flowLayoutPanel2.Controls.Add(this.textBoxDesc);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 38);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(334, 44);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Task Description";
+            // 
+            // textBoxDesc
+            // 
+            this.textBoxDesc.Location = new System.Drawing.Point(3, 16);
+            this.textBoxDesc.Multiline = true;
+            this.textBoxDesc.Name = "textBoxDesc";
+            this.textBoxDesc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDesc.Size = new System.Drawing.Size(328, 28);
+            this.textBoxDesc.TabIndex = 1;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.label1);
+            this.flowLayoutPanel4.Controls.Add(this.checkedListBoxFavorites);
+            this.flowLayoutPanel4.Controls.Add(this.buttonAddFav);
+            this.flowLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(3, 88);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(334, 183);
+            this.flowLayoutPanel4.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected Favorites";
+            // 
+            // checkedListBoxFavorites
+            // 
+            this.checkedListBoxFavorites.FormattingEnabled = true;
+            this.checkedListBoxFavorites.Location = new System.Drawing.Point(3, 16);
+            this.checkedListBoxFavorites.Name = "checkedListBoxFavorites";
+            this.checkedListBoxFavorites.Size = new System.Drawing.Size(328, 124);
+            this.checkedListBoxFavorites.TabIndex = 8;
+            // 
+            // buttonAddFav
+            // 
+            this.buttonAddFav.Location = new System.Drawing.Point(3, 146);
+            this.buttonAddFav.Name = "buttonAddFav";
+            this.buttonAddFav.Size = new System.Drawing.Size(95, 25);
+            this.buttonAddFav.TabIndex = 7;
+            this.buttonAddFav.Text = "Add Favorite";
+            this.buttonAddFav.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel5
+            // 
+            this.flowLayoutPanel5.Controls.Add(this.label2);
+            this.flowLayoutPanel5.Controls.Add(this.checkedListBoxCommands);
+            this.flowLayoutPanel5.Controls.Add(this.buttonAddCommand);
+            this.flowLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel5.Location = new System.Drawing.Point(3, 277);
+            this.flowLayoutPanel5.Name = "flowLayoutPanel5";
+            this.flowLayoutPanel5.Size = new System.Drawing.Size(334, 170);
+            this.flowLayoutPanel5.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Selected Commands";
+            // 
+            // checkedListBoxCommands
+            // 
+            this.checkedListBoxCommands.FormattingEnabled = true;
+            this.checkedListBoxCommands.Location = new System.Drawing.Point(3, 16);
+            this.checkedListBoxCommands.Name = "checkedListBoxCommands";
+            this.checkedListBoxCommands.Size = new System.Drawing.Size(328, 124);
+            this.checkedListBoxCommands.TabIndex = 9;
+            // 
+            // buttonAddCommand
+            // 
+            this.buttonAddCommand.Location = new System.Drawing.Point(3, 146);
+            this.buttonAddCommand.Name = "buttonAddCommand";
+            this.buttonAddCommand.Size = new System.Drawing.Size(95, 25);
+            this.buttonAddCommand.TabIndex = 2;
+            this.buttonAddCommand.Text = "Add Command";
+            this.buttonAddCommand.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel8
+            // 
+            this.flowLayoutPanel8.Controls.Add(this.Cancel);
+            this.flowLayoutPanel8.Controls.Add(this.buttonOK);
+            this.flowLayoutPanel8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel8.Location = new System.Drawing.Point(3, 453);
+            this.flowLayoutPanel8.Name = "flowLayoutPanel8";
+            this.flowLayoutPanel8.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.flowLayoutPanel8.Size = new System.Drawing.Size(334, 35);
+            this.flowLayoutPanel8.TabIndex = 10;
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(256, 3);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 30);
+            this.Cancel.TabIndex = 1;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(175, 3);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 30);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // Task_Edit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 490);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Task_Edit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Task_Edit";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.flowLayoutPanel5.ResumeLayout(false);
+            this.flowLayoutPanel5.PerformLayout();
+            this.flowLayoutPanel8.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDesc;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel8;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button buttonAddFav;
+        private System.Windows.Forms.Button buttonAddCommand;
+        private System.Windows.Forms.CheckedListBox checkedListBoxFavorites;
+        private System.Windows.Forms.CheckedListBox checkedListBoxCommands;
+    }
+}
---
+++ RemoteWork/RemoteWork/Managers/Task Edit.cs
@@ -0,0 +1,197 @@
+﻿using RemoteWork.Access;
+using RemoteWork.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace RemoteWork.Managers
+{
+    enum TaskInputValidate
+    {
+        TaskNameIsEmpty,
+        TaskFavsNotChecked,
+        TaskCommandsNotChecked
+    }
+    public partial class Task_Edit : Form
+    {
+        RconfigContext context = new RconfigContext();
+        WindowsMode mode = WindowsMode.ADD;
+        TaskInputValidate validateInput = TaskInputValidate.TaskNameIsEmpty;
+        RemoteTask currentTask;
+        int prevTaskId;
+        public Task_Edit()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        public Task_Edit(int taskId)
+        {
+            InitializeComponent();
+            mode = WindowsMode.EDIT;
+            prevTaskId = taskId;
+            LoadData();
+            LoadPrevData();
+        }
+
+        private async void LoadData()
+        {
+            var queryFavs=await (from c in context.Favorites
+                          select c).ToListAsync();
+
+            foreach (Favorite fav in queryFavs)
+            {
+                checkedListBoxFavorites.Items.Add(fav.Hostname);
+            }
+
+            var queryCommands = await (from c in context.Commands
+                                      select c.Name).ToListAsync();
+
+            foreach (string command in queryCommands)
+            {
+                checkedListBoxCommands.Items.Add(command);
+            }
+        }
+        private async void LoadPrevData()
+        {            
+            var queryRemoteTask=await (from c in context.RemoteTasks
+                                where c.Id==prevTaskId
+                                select c).FirstOrDefaultAsync();
+            if (queryRemoteTask != null)
+            {
+                currentTask = queryRemoteTask;
+            }
+            textBoxName.Text = currentTask.TaskName;
+            textBoxDesc.Text = currentTask.Description;
+        }
+
+        private bool CheckData()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                return false;
+            }
+            if (checkedListBoxFavorites.CheckedItems.Count == 0)
+            {
+                validateInput = TaskInputValidate.TaskFavsNotChecked;
+                return false;
+            }
+            if (checkedListBoxCommands.CheckedItems.Count == 0)
+            {
+                validateInput = TaskInputValidate.TaskCommandsNotChecked;
+                return false;
+            }
+            return true;
+        }
+
+        private void TaskAdd()
+        {
+            currentTask = new RemoteTask();
+            currentTask.TaskName = textBoxName.Text.Trim();
+            currentTask.Description=textBoxDesc.Text.Trim();
+            currentTask.Date = DateTime.UtcNow;
+
+            HashSet<Favorite> favs = new HashSet<Favorite>();
+            foreach (var item in checkedListBoxFavorites.CheckedItems)
+            {
+                string favorite = item.ToString();
+                var queryFav=(from c in context.Favorites
+                             where c.Hostname==favorite
+                             select c).FirstOrDefault();
+
+                if (queryFav != null)
+                    favs.Add(queryFav);
+            }
+            currentTask.Favorites = favs;
+            HashSet<Command> commands = new HashSet<Command>();
+            foreach (var item in checkedListBoxCommands.CheckedItems)
+            {
+                string command = item.ToString();
+                var queryCommand = (from c in context.Commands
+                                where c.Name== command
+                                select c).FirstOrDefault();
+
+                if (queryCommand != null)
+                    commands.Add(queryCommand);
+            }
+            currentTask.Commands = commands;
+            context.RemoteTasks.Add(currentTask);
+            context.SaveChanges();
+        }
+        private void TaskEdit()
+        {           
+            currentTask.TaskName = textBoxName.Text.Trim();
+            currentTask.Description = textBoxDesc.Text.Trim();
+            currentTask.Date = DateTime.UtcNow;
+
+            HashSet<Favorite> favs = new HashSet<Favorite>();
+            foreach (var item in checkedListBoxFavorites.CheckedItems)
+            {
+                string favorite = item.ToString();
+                var queryFav = (from c in context.Favorites
+                                where c.Hostname == favorite
+                                select c).FirstOrDefault();
+
+                if (queryFav != null)
+                    favs.Add(queryFav);
+            }
+            currentTask.Favorites = favs;
+            HashSet<Command> commands = new HashSet<Command>();
+            foreach (var item in checkedListBoxCommands.CheckedItems)
+            {
+                string command = item.ToString();
+                var queryCommand = (from c in context.Commands
+                                    where c.Name == command
+                                    select c).FirstOrDefault();
+
+                if (queryCommand != null)
+                    commands.Add(queryCommand);
+            }
+            currentTask.Commands = commands;
+            context.Entry(currentTask).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            if (CheckData())
+            {
+                switch (mode)
+                {
+                    case WindowsMode.ADD: TaskAdd(); break;
+                    case WindowsMode.EDIT: TaskEdit(); break;
+                }
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                switch (validateInput)
+                {
+                    case TaskInputValidate.TaskNameIsEmpty: NotifyWarning("Task Name input is empty!"); break;
+                    case TaskInputValidate.TaskFavsNotChecked: NotifyWarning("Favorites for task not checked!"); break;
+                    case TaskInputValidate.TaskCommandsNotChecked: NotifyWarning("Commands for task not checked!"); break;
+                }
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void NotifyWarning(string warning)
+        {
+            MessageBox.Show(warning, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+     
+
+
+
+    }
+}
