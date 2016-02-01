0a2edd12918ffc3df5aba59b5f1bff599e03e2df
Dauren Amanbayev <daur_aman@mail.ru>
2016-02-01 17:07:55 +0600

    Task Manager Added

---
+++ RemoteWork/RemoteWork/Managers/Task Manager.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace RemoteWork.Managers
+{
+    partial class Task_Manager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listViewDetails = new System.Windows.Forms.ListView();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.columnHeaderID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderTaskName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDesc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderCommands = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderFavCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 90F));
+            this.tableLayoutPanel1.Controls.Add(this.listViewDetails, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(667, 413);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // listViewDetails
+            // 
+            this.listViewDetails.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderID,
+            this.columnHeaderTaskName,
+            this.columnHeaderDesc,
+            this.columnHeaderCommands,
+            this.columnHeaderFavCount,
+            this.columnHeaderDate});
+            this.listViewDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewDetails.Location = new System.Drawing.Point(3, 3);
+            this.listViewDetails.MultiSelect = false;
+            this.listViewDetails.Name = "listViewDetails";
+            this.listViewDetails.Size = new System.Drawing.Size(571, 407);
+            this.listViewDetails.TabIndex = 0;
+            this.listViewDetails.UseCompatibleStateImageBehavior = false;
+            this.listViewDetails.View = System.Windows.Forms.View.Details;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.buttonCreate);
+            this.flowLayoutPanel1.Controls.Add(this.buttonEdit);
+            this.flowLayoutPanel1.Controls.Add(this.buttonDelete);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(580, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(84, 407);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(3, 3);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 34);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create Task";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(3, 43);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 34);
+            this.buttonEdit.TabIndex = 1;
+            this.buttonEdit.Text = "Edit Task";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(3, 83);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 34);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete Task";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // columnHeaderID
+            // 
+            this.columnHeaderID.Text = "Task ID";
+            // 
+            // columnHeaderTaskName
+            // 
+            this.columnHeaderTaskName.Text = "Task Name";
+            this.columnHeaderTaskName.Width = 85;
+            // 
+            // columnHeaderDesc
+            // 
+            this.columnHeaderDesc.Text = "Description";
+            this.columnHeaderDesc.Width = 95;
+            // 
+            // columnHeaderCommands
+            // 
+            this.columnHeaderCommands.Text = "Command Count";
+            this.columnHeaderCommands.Width = 95;
+            // 
+            // columnHeaderFavCount
+            // 
+            this.columnHeaderFavCount.Text = "Favorite Count";
+            this.columnHeaderFavCount.Width = 100;
+            // 
+            // columnHeaderDate
+            // 
+            this.columnHeaderDate.Text = "Date";
+            this.columnHeaderDate.Width = 120;
+            // 
+            // Task_Manager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 413);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Task_Manager";
+            this.Text = "Task_Manager";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView listViewDetails;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ColumnHeader columnHeaderID;
+        private System.Windows.Forms.ColumnHeader columnHeaderTaskName;
+        private System.Windows.Forms.ColumnHeader columnHeaderDesc;
+        private System.Windows.Forms.ColumnHeader columnHeaderCommands;
+        private System.Windows.Forms.ColumnHeader columnHeaderFavCount;
+        private System.Windows.Forms.ColumnHeader columnHeaderDate;
+    }
+}
---
+++ RemoteWork/RemoteWork/Managers/Task Manager.cs
@@ -0,0 +1,128 @@
+﻿using RemoteWork.Access;
+using RemoteWork.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace RemoteWork.Managers
+{
+    public partial class Task_Manager : Form
+    {
+        RconfigContext context = new RconfigContext();
+
+        public Task_Manager()
+        {
+            InitializeComponent();
+            LoadData();
+            //TaskAddTest();
+        }
+
+        private async  void LoadData()
+        {
+            var queryTasks=await  (from c in context.RemoteTasks
+                           select c).ToListAsync();
+
+            if (queryTasks != null)
+            {
+
+                foreach (RemoteTask task in queryTasks)
+                {
+                    var item = new ListViewItem(new [] { 
+                    task.Id.ToString(),
+                    task.TaskName,
+                    task.Description,
+                    task.Commands.Count.ToString(),
+                    task.Favorites.Count.ToString(),
+                    task.Date.ToString()});
+
+                    listViewDetails.Items.Add(item);
+                }
+            }
+        }
+        //удалить тестовый метод
+        private void TaskAddTest()
+        {
+            var item = new ListViewItem(new[] { 
+                    "1",
+                    "LoadConf",
+                    "Cherry",
+                    "show run",
+                    "25",
+                    DateTime.UtcNow.ToString()});
+
+            listViewDetails.Items.Add(item);
+            var item2 = new ListViewItem(new[] { 
+                    "2",
+                    "LoadConf",
+                    "Cherry",
+                    "show run",
+                    "25",
+                    DateTime.UtcNow.ToString()});
+            listViewDetails.Items.Add(item2);
+            var item3 = new ListViewItem(new[] { 
+                    "3",
+                    "LoadConf",
+                    "Cherry",
+                    "show run",
+                    "25",
+                    DateTime.UtcNow.ToString()});
+            listViewDetails.Items.Add(item3);
+        }
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Task_Edit frm = new Task_Edit();
+            DialogResult result = frm.ShowDialog();
+            if(result==DialogResult.OK)
+            {
+                listViewDetails.Items.Clear();
+                LoadData();
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (listViewDetails.SelectedItems.Count != 0)
+            {
+                var item = listViewDetails.SelectedItems[0];
+                int taskId = Int32.Parse(item.SubItems[0].Text);
+                Task_Edit frm = new Task_Edit(taskId);
+                DialogResult result = frm.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    listViewDetails.Items.Clear();
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listViewDetails.SelectedItems.Count != 0)
+            {
+                var item = listViewDetails.SelectedItems[0];
+                int taskId = Int32.Parse(item.SubItems[0].Text);
+                //MessageBox.Show(taskId.ToString());
+                var queryTask=(from c in context.RemoteTasks
+                              where c.Id==taskId
+                              select c).FirstOrDefault();
+
+                if (queryTask != null)
+                {
+                    context.RemoteTasks.Remove(queryTask);
+                    context.SaveChanges();
+                    listViewDetails.Items.Clear();
+                    LoadData();
+                }
+            }
+        }
+
+
+    }
+}
