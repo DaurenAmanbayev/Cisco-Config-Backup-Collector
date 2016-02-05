b6e074f65072a17088e8327ee776150d14af4930
Dauren Amanbayev <daur_aman@mail.ru>
2016-02-05 11:08:54 +0600

    multithreading problem resolve

--- RemoteWork/RemoteWorkUtil/favoriteConnect.cs
+++ RemoteWork/RemoteWorkUtil/favoriteConnect.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 
 namespace RemoteWorkUtil
 {
+    //реализовать перегрузку присваивания для создания полной копии объекта
+    //не ссылаясь на объект
+    //https://msdn.microsoft.com/en-us/library/aa288467%28v=vs.71%29.aspx
+    //можно использовать objectcopier 
+    //нужно выгрузить данные, чтобы они были не в базе данных, а локальными
+    //затем производить по ним требуемые операции
+    //перенести данный класс в RemoteWork.Data
     struct FavoriteConnect
     {
         public Favorite favorite;
