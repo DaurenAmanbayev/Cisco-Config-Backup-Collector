d14a3b7b5bff218daebf921e936b24c0c8e67333
Dauren Amanbayev <daur_aman@mail.ru>
2016-06-28 21:11:24 +0600

    Веб оболочка для проекта в шаблоне MVC

---
+++ RemoteWork/WebAppRemoteConfig/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppRemoteConfig.Controllers
+{
+    /*
+     * веб оболочка для системы сбора конфигураций с устройств
+     * возможно для более корректного сбора конфигураций, посмотреть имплементации коллекторов на других языках
+     */
+    public class HomeController: Controller
+    {
+
+    }
+}
---
+++ RemoteWork/WebAppRemoteConfig/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppRemoteConfig.MvcApplication" Language="C#" %>
