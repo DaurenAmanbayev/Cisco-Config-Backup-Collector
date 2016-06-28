90fd18f4c8161e764de6009ca3d4fc235d6e87fe
Dauren Amanbayev <daur_aman@mail.ru>
2016-06-28 21:14:07 +0600

    Добавлена имплементация коллектора

---
+++ RemoteWork/RemoteWork.Collector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteWork.Collector
+{
+    /*
+     * утилита для сбора конфигураций по указанному файлу устройств для быстрого тестирования
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
