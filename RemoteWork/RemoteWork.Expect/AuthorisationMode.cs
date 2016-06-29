d9a144216997ef132b031cf767420c034bea679c
Dauren Amanbayev <daur_aman@mail.ru>
2016-06-29 10:48:39 +0600

    Изменения в библиотеке добавлен метод анонимного подключения для Телнет

---
+++ RemoteWork/RemoteWork.Expect/AuthorisationMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteWork.Expect
+{
+    enum AuthorisationMode
+    {
+        Anonymous,//когда не требуется пользователь
+        AnonymousWithEnable,//анониманая с повышением привилегий
+        Simple,//обычная авторизация
+        SimpleWithEnable//обычная авторизация с повышением привилегий
+    }
+}
