01e87d0ba5d76dc2ac8112e7e51a4de4825f8bb2
Dauren Amanbayev <daur_aman@mail.ru>
2016-06-29 15:42:35 +0600

    Добавлен окно прогресс бара для режима ожидания выполнение процесса сбора данных

---
+++ RemoteWork/RemoteWork/WaitWindowGUI.Designer.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mjackson
+ * Date: 05/03/2010
+ * Time: 09:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace RemoteWork
+{
+	partial class WaitWindowGUI
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.Marque = new System.Windows.Forms.ProgressBar();
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Marque
+            // 
+            this.Marque.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Marque.Location = new System.Drawing.Point(12, 46);
+            this.Marque.MarqueeAnimationSpeed = 1;
+            this.Marque.Name = "Marque";
+            this.Marque.Size = new System.Drawing.Size(276, 12);
+            this.Marque.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.Marque.TabIndex = 0;
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MessageLabel.BackColor = System.Drawing.Color.Transparent;
+            this.MessageLabel.Location = new System.Drawing.Point(12, 12);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(276, 23);
+            this.MessageLabel.TabIndex = 1;
+            this.MessageLabel.Text = "Please wait ...";
+            this.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // WaitWindowGUI
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(300, 70);
+            this.Controls.Add(this.MessageLabel);
+            this.Controls.Add(this.Marque);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WaitWindowGUI";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "WaitWindowGUI";
+            this.ResumeLayout(false);
+
+		}
+		public System.Windows.Forms.Label MessageLabel;
+		private System.Windows.Forms.ProgressBar Marque;
+	}
+}
