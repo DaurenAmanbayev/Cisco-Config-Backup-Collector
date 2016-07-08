b71ddcb144c876bc25e6fc991a985a233f2b9dbc
Dauren Amanbayev <daur_aman@mail.ru>
2016-07-08 12:52:19 +0600

    Добавлена страница входа и привязки к стилям и скриптам

---
+++ RemoteWork/WebAppRemoteConfig/Filters/InitializeSimpleMembershipAttribute.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebAppRemoteConfig.Models;
+using WebMatrix.WebData;
+
+namespace WebAppRemoteConfig.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class InitializeSimpleMembershipAttribute : ActionFilterAttribute
+    {
+        private static SimpleMembershipInitializer _initializer;
+        private static object _initializerLock = new object();
+        private static bool _isInitialized;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Инициализация ASP.NET Simple Membership происходит один раз при старте приложения
+            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+        }
+
+        private class SimpleMembershipInitializer
+        {
+            public SimpleMembershipInitializer()
+            {
+                Database.SetInitializer<UsersContext>(null);
+
+                try
+                {
+                    using (var context = new UsersContext())
+                    {
+                        if (!context.Database.Exists())
+                        {
+                            // Создание базы данных SimpleMembership без применения миграции Entity Framework
+                            ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                        }
+                    }
+                    // Настройка  ASP.NET Simple Membership
+                    // 1 параметр - имя строки подключения к базе данных.
+                    // 2 параметр - таблица, которая содержит информацию о пользователях
+                    // 3 параметр - имя колонки в таблице, которая отвечает за хранение логина
+                    // 4 параметр - autoCreateTables автоматическое создание таблиц если они не существуют в базе
+                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+
+                    SimpleRoleProvider roles = (SimpleRoleProvider)Roles.Provider;
+                    SimpleMembershipProvider membership = (SimpleMembershipProvider)Membership.Provider;
+
+                    // Проверка наличия роли Moderator
+                    if (!roles.RoleExists("Moderator"))
+                    {
+                        roles.CreateRole("Moderator");
+                    }
+                    // Проверка наличия роли Admin
+                    if (!roles.RoleExists("Admin"))
+                    {
+                        roles.CreateRole("Admin");
+                    }
+                    // Поиск пользователя с логином admin
+                    if (membership.GetUser("admin", false) == null)
+                    {
+                        membership.CreateUserAndAccount("admin", "qwe123"); // создание пользователя
+                        roles.AddUsersToRoles(new[] { "admin" }, new[] { "Admin" }); // установка роли для пользователя
+                    }
+                    if (membership.GetUser("user1", false) == null)
+                    {
+                        membership.CreateUserAndAccount("user1", "qwe123");
+                        roles.AddUsersToRoles(new[] { "user1" }, new[] { "Moderator" });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+                }
+            }
+        }
+    }
+}
---
+++ RemoteWork/WebAppRemoteConfig/Models/UsersContext.cs
@@ -0,0 +1,55 @@
+﻿using RemoteWork.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+
+namespace WebAppRemoteConfig.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
+        }
+        //USER PROFILE MODEL
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        
+        //REMOTE WORK MODEL
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Credential> Credentials { get; set; }
+        public DbSet<Config> Configs { get; set; }
+        public DbSet<RemoteTask> RemoteTasks { get; set; }
+        public DbSet<Protocol> Protocols { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<Command> Commands { get; set; }
+        public DbSet<Location> Locations { get; set; }    
+    }
+}
